f6045fcee7d81be1c8ffd3600b55a5d5fc7e3c20
daudalvologic <daud@alvologic.com>
2019-04-04 17:35:35 +0530

    schema file

---
+++ schema/schema.sql
@@ -0,0 +1,6 @@
+/*
+For example, files could be named using sequence number:
+001_add_discounted_amount_to_products.sql
+002_add_total_amount_to_orders.sql
+003_patch_products_default_discounted_amount.sql
+*/
