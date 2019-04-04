1010f89d575f7b4a36848d00b4534bb7c56c3fcd
daudalvologic <daud@alvologic.com>
2019-04-04 17:31:45 +0530

    add dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //console.log('The value for FOO is:', process.env.AT);
   res.render('index', { title: 'Express' });
 });
 
