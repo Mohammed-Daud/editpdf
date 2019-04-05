3f399e2db859e577a158b11b247ed896cdfae55b
Pradeep Chaudhari <pradeep.chaudhari91@gmail.com>
2019-04-05 11:32:36 +0530

    add schema

--- config/database.js
+++ config/database.js
@@ -1,12 +1,27 @@
-
-var knex = require('knex')({
-    client: 'mysql',
+const knex = require("knex")({
+    client: "mysql",
     connection: {
-        host: '127.0.0.1',
-        user: 'your_database_user',
-        password: 'your_database_password',
-        database: 'myapp_test'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE_NAME
+    },
+    log: {
+        warn(message) {
+            console.log("Knex: warn -> message", message);
+        },
+        error(message) {
+            console.log("Knex: error -> message", message);
+        },
+        deprecate(message) {
+            console.log("Knex: deprecate -> message", message);
+        },
+        debug(message) {
+            console.log("Knex: debug -> message", message);
+        }
     }
 });
 
-module.exports = {knex}
+module.exports = {
+    knex
+};
